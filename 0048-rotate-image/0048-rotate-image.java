e4f64681c31a90b6dcd1256c78e7afb7437828ea
Akash Aggarwal <akashaggarwal2105@gmail.com>
2026-09-12 02:47:45 +0530

    Time: 0 ms (100%), Space: 44 MB (16.5%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,6 +2,7 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
+        // Step 1 : Transpose 
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 int temp = matrix[i][j];
@@ -10,6 +11,7 @@ class Solution {
             }
         }
 
+        // Step 2 : reverse each row
         for(int i=0; i<n; i++){
             int left = 0;
             int right = n-1;
@@ -21,7 +23,7 @@ class Solution {
                 left++;
                 right--;
             }
-        }
 
+        }
     }
 }
